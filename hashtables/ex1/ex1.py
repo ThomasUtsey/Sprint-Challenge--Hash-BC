1f883ff16b85b606fecba1c575983fa68b7c4748
thomas utsey <tomutsey@yahoo.com>
2019-10-14 21:48:22 -0400

    Debug and complete hash tables.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,55 +8,18 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-    for index in range(length):
-        hash_table_insert(ht,index,(weights[index],limit-weights[index]))
-    for index in range(length):
-        weight, needed_weight = hash_table_retrieve(ht,index)
-
-        for jndex in range(index+1,length):
-            weights, _ = hash_table_retrieve(ht,jndex)
-
-            if weight == needed_weight:
-                if index > jndex:
-                    return (index,jndex)
-                else:
-                    return (jndex,index)
-                            
-                #  return index,jndex if index > jndex else jndex,index
-
-
-
-    return None
-    def get_indices_of_item_weights_(weights, length, limit):
-        ht = HashTable(16)
-        for index in range(length):
-            hash_table_insert(ht,weight,index)
     
-        for index in range(length):
-            weight = weights[index]
-            first = hash_table_retrieve(ht,weight)
-            needed_weight = limit - weight
-            second = hash_table_retrieve(ht,needed_weight)
-            hash_table_insert(ht, weight, index)
-
-            for jndex in range(index+1,length):
-                weights, _ = hash_table_retrieve(ht,jndex)
-
-            if second:
-                if first > second:
-                    return (first,second)
-                else:
-                    return (second,first)
-                            
-                #  return index,jndex if index > jndex else jndex,index
+    for i in range(0, length):
+        hash_table_insert(ht, weights[i], i)
 
 
+    for i in range(0, length):
+        result = hash_table_retrieve(ht, (limit - weights[i]))
+        if result is not None:
+            if result > i:
+                resultSet = (result, i)
+            else:
+                resultSet = (i, result)
+            return resultSet
 
     return None
-
-
-def print_answer(answer):
-    if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
-    else:
-        print("None")
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,15 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
+    location = hash_table_retrieve(hashtable, "NONE")
+
+    for i in range(length):
+        route[i] = location
+        location = hash_table_retrieve(hashtable, location)
+
+    print(route)
     return route
+
